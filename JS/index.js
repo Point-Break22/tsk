6b434744efc1133d0b3849af789a01f4ea07f70c
Point-Break22 <crystalsjgayle@gmail.com>
2022-08-25 11:00:32 -0400

    .

---
+++ JS/index.js
@@ -0,0 +1,28 @@
+<!-- jquery for color change navbar on scroll -->
+    <script>
+
+        $(document).ready(function(){
+            $(window).scroll(function(){
+               var scroll = $(window).scrollTop();
+               if(scroll > 280){
+                $(".navbar-dark").css("background", "#1db1a8");
+               } 
+               else{
+                $(".navbar-dark").css("background" , "transparent");
+               }
+            });
+        });
+    </script>
+
+@media min width (whatever mobile size is) --- anything below 600 is display: none; 
+display:; block;
+
+max width 600, display none
+
+
+
+
+
+
+
+
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# tsk
- 
+# Group-Project-6
+PPGM Group Project - group # 6
